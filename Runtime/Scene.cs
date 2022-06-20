a09f5ff488e1644cc72e3c24d71b5253c66a668c
wellshsu <wellshsu@outlook.com>
2022-06-20 11:56:58 +0800

    initial commit

---
+++ Runtime/Scene.cs
@@ -0,0 +1,21 @@
+﻿//---------------------------------------------------------------------//
+//                    GNU GENERAL PUBLIC LICENSE                       //
+//                       Version 2, June 1991                          //
+//                                                                     //
+// Copyright (C) Wells Hsu, wellshsu@outlook.com, All rights reserved. //
+// Everyone is permitted to copy and distribute verbatim copies        //
+// of this license document, but changing it is not allowed.           //
+//                  SEE LICENSE.md FOR MORE DETAILS.                   //
+//---------------------------------------------------------------------//
+namespace EP.U3D.LIBRARY.SCENE
+{
+    public abstract class Scene
+    {
+        public bool Inited = false;
+        public virtual string Name() { return string.Empty; }
+        public virtual void Awake() { }
+        public virtual void Start() { }
+        public virtual void Update() { }
+        public virtual void Stop() { }
+    }
+}
---
+++ Runtime/SceneManager.cs
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------//
+//                    GNU GENERAL PUBLIC LICENSE                       //
+//                       Version 2, June 1991                          //
+//                                                                     //
+// Copyright (C) Wells Hsu, wellshsu@outlook.com, All rights reserved. //
+// Everyone is permitted to copy and distribute verbatim copies        //
+// of this license document, but changing it is not allowed.           //
+//                  SEE LICENSE.md FOR MORE DETAILS.                   //
+//---------------------------------------------------------------------//
+using EP.U3D.LIBRARY.BASE;
+using System;
+
+namespace EP.U3D.LIBRARY.SCENE
+{
+    public static class SceneManager
+    {
+        public static Scene Last;
+        public static Scene Current;
+        public static Scene Next;
+        public static event Action<Scene, Scene> OnSwap;
+
+        public static void Update()
+        {
+            Current?.Update();
+            if (Next != null)
+            {
+                Current?.Stop();
+                Last = Current;
+                Current = Next;
+                Next = null;
+                Current?.Start();
+                OnSwap?.Invoke(Last, Current);
+            }
+        }
+
+        public static void Goto(Scene scene)
+        {
+            if (!scene.Inited)
+            {
+                scene.Inited = true;
+                scene.Awake();
+            }
+            if (Next != null && scene.Name() == Next.Name()) return;
+            if ((Current != null && Current.Name() != scene.Name()) || Current == null)
+            {
+                Next = scene;
+            }
+            else
+            {
+                Helper.LogWarning(Constants.RELEASE_MODE ? null : "Goto: can not go to same scene.");
+            }
+        }
+    }
+}
